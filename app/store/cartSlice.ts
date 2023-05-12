dc11a1bf607108f4fe810a85774d0ea5a5c5027d
hungtq1016 <hungtq1016@gmail.com>
2023-05-12 23:54:00 +0700

    fix

--- app/store/cartSlice.ts
+++ app/store/cartSlice.ts
@@ -5,9 +5,9 @@ import { PayloadAction, createSlice } from '@reduxjs/toolkit';
 export interface ICartState{
     cartItems : Array<ICartItem>
 }
-const carts:ICartItem[] = JSON.parse(localStorage.getItem('carts') || '[]');
+// const carts:ICartItem[] = JSON.parse(localStorage.getItem('carts') || '[]');
 
-const initialState: ICartState = {cartItems:carts};
+const initialState: ICartState = {cartItems:[]};
 
 export const cartSlice = createSlice({
     name: "cart",
@@ -22,7 +22,7 @@ export const cartSlice = createSlice({
                 product: action.payload.product,
                 inCart:action.payload.inCart,
             })
-            localStorage.setItem('carts', JSON.stringify(state.cartItems));
+            // localStorage.setItem('carts', JSON.stringify(state.cartItems));
         },
         decrement:(state,action:PayloadAction<string>)=>{
             const item = state.cartItems.find(item => item.product.id == action.payload)
@@ -34,18 +34,18 @@ export const cartSlice = createSlice({
                         return item.product.id !== action.payload
                     })
             }
-            localStorage.setItem('carts', JSON.stringify(state.cartItems));
+            // localStorage.setItem('carts', JSON.stringify(state.cartItems));
         },
         clearById:(state,action:PayloadAction<string>)=>{
             state.cartItems = state.cartItems.filter((item)=>{
                 return item.product.id !== action.payload
             })
-            localStorage.setItem('carts', JSON.stringify(state.cartItems));
+            // localStorage.setItem('carts', JSON.stringify(state.cartItems));
 
         },
         clearAll:(state)=>{
             state.cartItems = []
-            localStorage.setItem('carts', JSON.stringify(state.cartItems));
+            // localStorage.setItem('carts', JSON.stringify(state.cartItems));
         }
     }
 })
