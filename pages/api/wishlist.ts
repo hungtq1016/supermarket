a100a2de4db65a17981b2072eb3b26ae300ab540
hungtq1016 <hungtq1016@gmail.com>
2023-05-18 13:36:44 +0700

    delete method

--- pages/api/wishlist.ts
+++ pages/api/wishlist.ts
@@ -21,6 +21,8 @@ export default async function handler(req:NextApiRequest,res:NextApiResponse){
         case 'DELETE':
             try {
                 const dataDelete =req.body
+                console.log(dataDelete);
+                
                 await prisma.wishlist.delete({
                     where:{
                         id:dataDelete.id                    
