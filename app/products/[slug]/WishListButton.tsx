7226275aa532dcd58952a8b88f68020ce8fc58d1
hungtq1016 <hungtq1016@gmail.com>
2023-05-18 13:50:25 +0700

    ....

--- app/products/[slug]/WishListButton.tsx
+++ app/products/[slug]/WishListButton.tsx
@@ -20,7 +20,9 @@ export default function WishListButton({variantId,type}:{variantId:string,type:s
     }
 
     async function removeFromWishList(payload: any){
-        axios.delete(`/api/wishlist`, payload).then(
+        console.log(payload);
+        
+        axios.delete(`/api/wishlist`, { data: payload }).then(
             res=>setLike(false)
         ).catch(err=>console.log(err));
         console.log();
@@ -38,7 +40,7 @@ export default function WishListButton({variantId,type}:{variantId:string,type:s
     if(isLike) return  (
         <button  onClick={()=>removeFromWishList({id:found.id})}
         className={`${type=='product' ? 'border rounded-md py-2 px-2.5 border-red-600 bg-red-600':`${type == 'horizontal' ?'flex-1 bg-red-600':' '}`}`}>
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className={`${type=='product' ? 'w-6 h-6 ':'w-4 h-4'} ${type == 'horizontal' ?'text-red-600':''} mx-auto text-white`}>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className={`${type=='product' ? 'w-6 h-6 ':'w-4 h-4'} ${type == 'vertical' ?' text-red-600 ':' text-white '} mx-auto `}>
                 <path d="M11.645 20.91l-.007-.003-.022-.012a15.247 15.247 0 01-.383-.218 25.18 25.18 0 01-4.244-3.17C4.688 15.36 2.25 12.174 2.25 8.25 2.25 5.322 4.714 3 7.688 3A5.5 5.5 0 0112 5.052 5.5 5.5 0 0116.313 3c2.973 0 5.437 2.322 5.437 5.25 0 3.925-2.438 7.111-4.739 9.256a25.175 25.175 0 01-4.244 3.17 15.247 15.247 0 01-.383.219l-.022.012-.007.004-.003.001a.752.752 0 01-.704 0l-.003-.001z" />
             </svg>
         </button>
